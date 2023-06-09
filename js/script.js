f2f8c8add630a4bec0cd93c99b4b469cb4348c3c
Asif Iqbal <iasifiqbal.1998@gmail.com>
2023-06-09 12:15:05 +0530

    Activity and Students Work section updated, meta data upadted

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-var swiper = new Swiper(".mySwiper", {
+/*var swiper = new Swiper(".mySwiper", {
     slidesPerView: 1,
     spaceBetween: 30,
     loop: true,
@@ -10,4 +10,44 @@ var swiper = new Swiper(".mySwiper", {
       nextEl: ".swiper-button-next",
       prevEl: ".swiper-button-prev",
     },
-  });
+  });*/
+
+  var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 2.5,
+    spaceBetween: 10,
+    slidesPerGroup: 1,
+    loop: false,
+    loopFillGroupWithBlank: true,
+    breakpoints: {
+        1000: {
+            slidesPerView: 3,
+            spaceBetween: 15,
+        },
+
+        768: {
+            slidesPerView: 2.2,
+            spaceBetween: 20,
+        },
+        484: {
+            slidesPerView: 1.5,
+            spaceBetween: 20,
+        },
+        280: {
+            slidesPerView: 1.2,
+            spaceBetween: 20,
+        },
+        100: {
+            slidesPerView: 1,
+            spaceBetween: 10,
+        },
+    },
+
+
+    navigation: {
+        nextEl: "#swiper-button-next-1",
+        prevEl: "#swiper-button-prev-1",
+    },
+});
+
+
+
